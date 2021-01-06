b1940c62401d495a5b1bfef6f38aede3e045af3d
StragelX <stragelx@gmail.com>
2021-01-06 10:30:27 +0200

    fixes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,8 +3,13 @@ $( document ).ready(function() {
         $(this).closest(".cookies").addClass("hidden");
     })
 
-    var bg = document.getElementById('bg');
-    var parallaxInstance = new Parallax(bg, {
+    $(".phone").mask("+38(099) 999 99 99");
+
+    $('.placer').blur(function(){
+      ( $(this) ).val() !="" ? $(this).addClass("filled") : $(this).removeClass("filled");
+    })
+
+    var parallaxInstance = new Parallax(document.getElementById('bg'), {
       scalarX: 2.0,
       scalarY: 3.5
     });
@@ -18,5 +23,15 @@ $( document ).ready(function() {
           nextEl: '.swiper_next',
           prevEl: '.swiper_prev',
         },
-      })
+    })
+
+    $(".call_modal").click(function(){
+      $("body").addClass("show_modal");
+      $(".modal_overlay").removeClass("hidden");
+    })
+
+    $('.close_modal, .close').click(function(){
+      $(this).closest(".modal_overlay").addClass("hidden");
+      $("body").removeClass("show_modal");
+    })
 });
